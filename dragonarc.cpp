812c2f27fc84015bfffab20feffb0bd4fcc2dd43
Ace <99183587+vaibhav-ace@users.noreply.github.com>
2022-04-18 21:55:15 +0930

    Update dragonarc.cpp

--- dragonarc.cpp
+++ dragonarc.cpp
@@ -1,8 +1,8 @@
-#include "dragon.h"
+ #include "dragon.h"
 #include "player.h"
 #include "hero.h"
 #include "enemy.h"
-#include <climits>
+#include "input.h"
 #include <iostream>
 #include <stdlib.h>
 #include <string>
@@ -10,18 +10,17 @@
 using namespace std;
 
 //Hero global variable
-hero h3;
+static hero h1;
 
 //Function Names: (Defined after the end of dragonarc function)
 void heroAttack3();
-int getting_input3();
 
 void dragonarc(int health, int mana){
-    h3.set_player_mana(mana);
-    h3.set_player_health(health);
+    h1.set_player_mana(mana);
+    h1.set_player_health(health);
 
     //Dragon class
-    dragon* d1=new dragon(); //Prologueg
+    dragon* d1=new dragon(); //Prologue
     dragon* d2=new dragon(); //Health
     dragon* d3=new dragon();//Magic
 
@@ -54,16 +53,16 @@ void dragonarc(int health, int mana){
     cout << "Health: " << d2->get_health() << endl;
     cout << "Magic: " << d3->get_magic() << endl;
     cout << "++++++ 100 MANA +++++" << endl << "Player Mana now at 100" << endl << endl;
-    h3.set_player_mana(100);
+    h1.set_player_mana(100);
 
     //While loop to ensure player is alive while game continues
-    while (d2->get_health()>0 && h3.get_player_health() > 0){
+    while (d2->get_health()>0 && h1.get_player_health() > 0){
 
         heroAttack3();//Obtaining input
-        cout << "Player current hp:" << h3.get_player_health() << endl;
+        cout << "Player current hp:" << h1.get_player_health() << endl;
         //Status redefinition
-        d2->set_health(d2->get_health()-h3.get_health_dmg());
-        d3->set_magic(d3->get_magic()-h3.get_magic_dmg());
+        d2->set_health(d2->get_health()-h1.get_health_dmg());
+        d3->set_magic(d3->get_magic()-h1.get_magic_dmg());
         cout << "Dragon Prince health: " << d2->get_health() << endl;
         cout << "Dragon Prince magic: " << d3->get_magic() << endl;
 
@@ -73,54 +72,55 @@ void dragonarc(int health, int mana){
             //Attack 1
             if (random_3==1){
                 cout<<"You have been hit with "<<d5->get_attack_name()<<", -"<<d5->get_attack_health()<<" health and -"<<d5->get_attack_magic()<<" magic..."<<endl;
-                h3.set_player_health(h3.get_player_health()-d5->get_attack_health());
-                cout << "Player hp: " << h3.get_player_health() << endl;
-                h3.set_player_mana(h3.get_player_mana() - d5->get_attack_magic());
-                cout << "Player mana:" << h3.get_player_mana() << endl;
+                h1.set_player_health(h1.get_player_health()-d5->get_attack_health());
+                cout << "Player hp: " << h1.get_player_health() << endl;
+                h1.set_player_mana(h1.get_player_mana() - d5->get_attack_magic());
+                cout << "Player mana:" << h1.get_player_mana() << endl;
             }
 
             //Attack 2
             if (random_3==2){
                 cout<<"You have been hit with "<<d6->get_attack_name()<<", -"<<d6->get_attack_health()<<" health and -"<<d6->get_attack_magic()<<" magic..."<<endl;
-                h3.set_player_health(h3.get_player_health()-d6->get_attack_health());
-                cout << "Player hp: " << h3.get_player_health() << endl;
-                h3.set_player_mana(h3.get_player_mana() - d6->get_attack_magic());
-                cout << "Player mana:" << h3.get_player_mana() << endl;
+                h1.set_player_health(h1.get_player_health()-d6->get_attack_health());
+                cout << "Player hp: " << h1.get_player_health() << endl;
+                h1.set_player_mana(h1.get_player_mana() - d6->get_attack_magic());
+                cout << "Player mana:" << h1.get_player_mana() << endl;
             }
 
             //Attack 3
             if (random_3==3){
                 cout<<"You have been hit with "<<d7->get_attack_name()<<", -"<<d7->get_attack_health()<<" health and -"<<d7->get_attack_magic()<<" magic..."<<endl;
-                h3.set_player_health(h3.get_player_health()-d7->get_attack_health());
-                cout << "Player hp: " << h3.get_player_health() << endl;
-                h3.set_player_mana(h3.get_player_mana() - d7->get_attack_magic());
-                cout << "Player mana:" << h3.get_player_mana() << endl;
+                h1.set_player_health(h1.get_player_health()-d7->get_attack_health());
+                cout << "Player hp: " << h1.get_player_health() << endl;
+                h1.set_player_mana(h1.get_player_mana() - d7->get_attack_magic());
+                cout << "Player mana:" << h1.get_player_mana() << endl;
             }
 
             //Attack 4
             if (random_3==4){
                 cout<<"You have been hit with "<<d8->get_attack_name()<<", -"<<d8->get_attack_health()<<" health and -"<<d8->get_attack_magic()<<" magic..."<<endl;
-                h3.set_player_health(h3.get_player_health()-d8->get_attack_health());
-                cout << "Player hp: " << h3.get_player_health() << endl;
-                h3.set_player_mana(h3.get_player_mana() - d8->get_attack_magic());
-                cout << "Player mana:" << h3.get_player_mana() << endl;
+                h1.set_player_health(h1.get_player_health()-d8->get_attack_health());
+                cout << "Player hp: " << h1.get_player_health() << endl;
+                h1.set_player_mana(h1.get_player_mana() - d8->get_attack_magic());
+                cout << "Player mana:" << h1.get_player_mana() << endl;
             }
 
             //Attack 5
             if (random_3==5){
                 cout<<"You have been hit with "<<d9->get_attack_name()<<", -"<<d9->get_attack_health()<<" health and -"<<d9->get_attack_magic()<<" magic..."<<endl;
-                h3.set_player_health(h3.get_player_health()-d9->get_attack_health());
-                h3.set_player_mana(h3.get_player_mana() - d9->get_attack_magic());
-                cout << "Player hp: " << h3.get_player_health() << endl;
-                cout << "Player mana:" << h3.get_player_mana() << endl;
+                h1.set_player_health(h1.get_player_health()-d9->get_attack_health());
+                h1.set_player_mana(h1.get_player_mana() - d9->get_attack_magic());
+                cout << "Player hp: " << h1.get_player_health() << endl;
+                cout << "Player mana:" << h1.get_player_mana() << endl;
             }
         }
     }
     //Player health check
-    if(h3.get_player_health()<=0){
+    if(h1.get_player_health()<=0){
         cout << "Defeated" << endl;
         cout << "----------------------------------------------------------------------------"<<endl;
-        exit(EXIT_FAILURE);
+        //exit(EXIT_FAILURE);
+        return;
     }
     cout << "Successfully slayed the dragon prince!" << endl;
     cout << "----------------------------------------------------------------------------"<<endl;
@@ -139,59 +139,32 @@ void dragonarc(int health, int mana){
     delete d8;
     delete d9;
 
-
     return;
 }
 
 //Defining functions used for input attacks
 
-//Function: Used to get only valid input
-int getting_input3(){
-    int input;
-
-    while (1){
-        cin >> input;
-        //If value is not an integer
-        if (!cin){
-            cout << "Please enter a valid attack..." << endl;
-            cin.clear();
-            cin.ignore (INT_MAX, '\n');
-            continue;
-        }
-        //If value is not 1 or 2 for the defined attacks
-        if (input!=1 && input!=2){
-            cout << "Please enter a valid attack..." << endl;
-            continue;
-        }
-        //If obtained value is suitable
-        if (input==1 || input==2){
-            return input; //Exit function
-        }
-    }//End of while loop
-}
-
-
 //Function used to get input
 void heroAttack3(){
     repeat:
     cout << "Please enter attack: 1.Basic Attack 2.Fireball " << endl;
-    int input=getting_input3();//Going to the function above to check if the input is valid
+    int input=getting_input();//Going to the function above to check if the input is valid
 
     //Setting move
-    h3.setMove(input);
+    h1.setMove(input);
 
     //Setting attack values for input 1 and 2
-    if(h3.getMove() == 1){
-     h3.set_attack(100);
-     h3.get_attack();
-     h3.set_player_mana(h3.get_player_mana()-0);
-     cout<< "Player Current Mana: " << h3.get_player_mana() << endl;
+    if(h1.getMove() == 1){
+     h1.set_attack(100);
+     h1.get_attack();
+     h1.set_player_mana(h1.get_player_mana()-0);
+     cout<< "Player Current Mana: " << h1.get_player_mana() << endl;
 
     }
-    else if (h3.getMove() == 2 && h3.get_player_mana() > 0){
-        h3.setFirstSkill(200,200);
-        h3.set_player_mana(h3.get_player_mana()-10); //uses mana for each skill usage
-        cout<< "Player Current Mana: " << h3.get_player_mana() << endl;
+    else if (h1.getMove() == 2 && h1.get_player_mana() > 0){
+        h1.setFirstSkill(200,200);
+        h1.set_player_mana(h1.get_player_mana()-10); //uses mana for each skill usage
+        cout<< "Player Current Mana: " << h1.get_player_mana() << endl;
     }
     else {
         //Else case (when hero have no mana to input the required skill)
