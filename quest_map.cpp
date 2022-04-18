ad462a790e8cfb4484c64ec5e40a8384741e556b
Ace <99183587+vaibhav-ace@users.noreply.github.com>
2022-04-19 02:25:17 +0930

    Update quest_map.cpp

--- quest_map.cpp
+++ quest_map.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int quest_map(int &health, int &mana){
     //Options to choose a power up for the player
-    cout << endl << endl << endl << "----------------------------------------------------------------------------" << endl;
+    cout << endl << endl << endl << endl<< "----------------------------------------------------------------------------" << endl;
     cout << "Welcome to the Quest Map!" << endl << "Please enter a stat number from: 1. hp     2. mana     3. life     4. esc" <<endl;
 
     string input;
@@ -15,7 +15,7 @@ int quest_map(int &health, int &mana){
     if (input == "3"){cout << "Extra life obtained..."<<endl;}
     else{cout << "You have chosen nothing as your ally against the Dragon Prince... May the goddess of luck be with you brave warrior..." <<endl;}
 
-    cout << "----------------------------------------------------------------------------" << endl << endl << endl <<endl <<endl <<endl <<endl <<"----------------------------------------------------------------------------"<<endl;
+    cout << "----------------------------------------------------------------------------" << endl << endl << endl <<endl <<endl <<"----------------------------------------------------------------------------"<<endl;
     if (input=="3"){return 1;}
     return 0;
 }
