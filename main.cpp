05c76879702dc38cda0d8c488e9a0e33b871d48a
Ace <99183587+vaibhav-ace@users.noreply.github.com>
2022-04-19 02:17:39 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "knightarc.h"
 #include "magearc.h"
 #include "dragonarc.h"
+#include "quest_map.h"
 using namespace std;
 
 //Main Function: Execution of the game
@@ -11,7 +12,7 @@ int main(){
     //Game variables
     int health=0;int mana=0;
     vector <int> new_arc {health, mana};
-
+    int power=0;
 
     //Knight arc begins!
     new_arc=knightarc(new_arc[0], new_arc[1]);
@@ -23,7 +24,9 @@ int main(){
 
     if (new_arc[0]!=0){
         //Dragon arc begins!
+        power=quest_map(new_arc[0], new_arc[1]);
         new_arc=dragonarc(new_arc[0], new_arc[1]);
+        if (power==1 && new_arc[0]==0){dragonarc(2000,100);}//new life obtained
     }
 
     //Returning 0
