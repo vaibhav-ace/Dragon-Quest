eaf90669d2cc7986eecf11ae2181d9753f6f47ea
Ace <99183587+vaibhav-ace@users.noreply.github.com>
2022-04-19 02:19:53 +0930

    Add files via upload

---
+++ quest_map.cpp
@@ -0,0 +1,21 @@
+#include "quest_map.h"
+#include <iostream>
+using namespace std;
+
+int quest_map(int &health, int &mana){
+    //Options to choose a power up for the player
+    cout << endl << endl << endl << "----------------------------------------------------------------------------" << endl;
+    cout << "Welcome to the Quest Map!" << endl << "Please enter a stat number from: 1. hp     2. mana     3. life     4. esc" <<endl;
+
+    string input;
+    cin >> input;
+
+    if (input == "1"){health+=1000; cout << "Health +++1000+++" << endl;}
+    if (input == "2"){mana+=500; cout << "Mana +++500+++" <<endl;}
+    if (input == "3"){cout << "Extra life obtained..."<<endl;}
+    else{cout << "You have chosen nothing as your ally against the Dragon Prince... May the goddess of luck be with you brave warrior..." <<endl;}
+
+    cout << "----------------------------------------------------------------------------" << endl << endl << endl <<endl <<endl <<endl <<endl <<"----------------------------------------------------------------------------"<<endl;
+    if (input=="3"){return 1;}
+    return 0;
+}
---
+++ quest_map.h
@@ -0,0 +1,7 @@
+#ifndef QUEST_MAP_H
+#define QUEST_MAP_H
+
+//Quest Map for Power-Ups
+int quest_map(int &health, int &mana);
+
+#endif
