f6e6dffbade4fd39a0633ae295f8d8b19d9f1d70
Vaibhav-Ace <99183587+vaibhav-ace@users.noreply.github.com>
2022-05-18 17:27:11 +0930

    Update welcome.cpp

--- welcome.cpp
+++ welcome.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void welcome(){
     cout <<"\t\t\t\t\t\t\t#################################################"<<endl;
     cout <<"\t\t\t\t\t\t\t#####              DRAGON QUEST             #####"<<endl;
-    cout <<"\t\t\t\t\t\t\t#################################################\n\n\n"<<endl;
+    cout <<"\t\t\t\t\t\t\t#################################################"<<endl<<endl<<endl<<endl;
 
 
 
